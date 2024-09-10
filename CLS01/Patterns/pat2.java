949617a2b64a3ec351d5cb7d98895aee1d350b0e
Rishi Bhargawa <rishi.bhargawa23@st.niituniversity.in>
2024-09-10 11:45:12 +0530

    added

---
+++ CLS01/Patterns/pat2.java
@@ -0,0 +1,13 @@
+package Patterns;
+
+public class pat2 {
+    public static void main(String[] args) {
+        int i,j;
+        for(i=5;i>=1;i=i-1){
+            for(j=1;j<=i;j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
