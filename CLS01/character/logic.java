7f8ac36e0d357f6f65ab67a9a93f8b16875d2645
Rishi Bhargawa <rishi.bhargawa23@st.niituniversity.in>
2024-09-13 10:21:18 +0530

    new files

---
+++ CLS01/character/logic.java
@@ -0,0 +1,6 @@
+public class logic {
+    public void str(String s1){
+        int len=s1.length();
+        String line=s1.toLowerCase();
+    }
+}
---
+++ CLS01/character/mainchar.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class mainchar {
+    public static void main(String[] args) {
+        logic obj=new logic();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your Line: ");
+        String s1=sc.nextLine();
+        obj.str(s1);
+    }
+}
