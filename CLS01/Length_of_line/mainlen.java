7526822402001836fc2dc25c5cb9d59f6ee51d8b
Rishi Bhargawa <rishi.bhargawa23@st.niituniversity.in>
2024-09-17 15:08:51 +0530

    unknown

--- CLS01/Length_of_line/mainlen.java
+++ CLS01/Length_of_line/mainlen.java
@@ -1,14 +1,1 @@
 //WAP to accept 10 lines from user and display the second largest line.
-package Length_of_line;
-
-import java.util.Scanner;
-public class mainlen {
-
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int i;
-        for(i=1;i<=10;i++){
-            
-        }
-    }
-}
