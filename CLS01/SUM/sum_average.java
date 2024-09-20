4432f3067b4e9ada973204fc526a79ef912959e0
Rishi Bhargawa <rishi.bhargawa23@st.niituniversity.in>
2024-09-20 22:57:54 +0530

    .

--- CLS01/SUM/sum_average.java
+++ CLS01/SUM/sum_average.java
@@ -6,9 +6,9 @@ public class sum_average {
         //Scanner sc=new Scanner(System.in);
         //System.out.println("Enter your Numbers: ");
         int arr[]={2,4,5,7,8,9,27,89,21,56}; //Array Declaration
-        for(int i=0;i<arr.length;i++){ //Loop for Iteration
+        //for(int i=0;i<arr.length;i++){ //Loop for Iteration
           // arr[i]=sc.nextInt(); //Input in Array Indexes
-        }
+        //}
         int result =obj.sum(arr); //Defined result var to call method
         System.out.println("SUM is: "+result);
         System.out.println("Average is: "+result/(float)arr.length);
