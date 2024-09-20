9b4de720ed380bbb4d9c24ce3363d4a315b15e02
Rishi Bhargawa <rishi.bhargawa23@st.niituniversity.in>
2024-09-20 09:19:11 +0530

    new

--- CLS01/SUM/sum_average.java
+++ CLS01/SUM/sum_average.java
@@ -1,18 +1,18 @@
 package SUM;
-import java.util.Scanner;
+//import java.util.Scanner;
 public class sum_average {
     public static void main(String[] args) {
         sumlogic obj=new sumlogic();
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter your Numbers: ");
-        int arr[]=new int[5]; //Array Declaration
-        for(int i=0;i<5;i++){ //Loop for Iteration
-           arr[i]=sc.nextInt(); //Input in Array Indexes
+        //Scanner sc=new Scanner(System.in);
+        //System.out.println("Enter your Numbers: ");
+        int arr[]={2,4,5,7,8}; //Array Declaration
+        for(int i=0;i<arr.length;i++){ //Loop for Iteration
+          // arr[i]=sc.nextInt(); //Input in Array Indexes
         }
         int result =obj.sum(arr); //Defined result var to call method
         System.out.println("SUM is: "+result);
         System.out.println("Average is: "+result/5);
-        sc.close();   
+        //sc.close();   
     }
 }
 
--- CLS01/SUM/sumlogic.java
+++ CLS01/SUM/sumlogic.java
@@ -3,7 +3,7 @@ package SUM;
 public class sumlogic {
     int sum(int arr[]){ //Define Method
         int sum=0,i;
-        for(i=0;i<5;i++) //Loop for Iteration
+        for(i=0;i<arr.length;i++) //Loop for Iteration (Replace Arr.length with iterations for input based)
         {
             sum=sum+arr[i]; //SUM fx.
         }
