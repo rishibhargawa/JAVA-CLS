fb3487fc77759c7133f3775c084536b2275df315
Rishi Bhargawa <rishi.bhargawa23@st.niituniversity.in>
2024-09-23 16:22:12 +0530

    new

--- CLS01/Empl/Employee.java
+++ CLS01/Empl/Employee.java
@@ -1,20 +1,20 @@
 package Empl;
 
 public class Employee {
-    String name1;
+    String name;
     int id;
     int age;
     String dept;
 
     Employee(String name,int id,int age,String dept){
-        name1 = name;
+        this.name = name;
         this.id=id;
         this.age=age;
         this.dept=dept;
     }
 
-    @Override
+   
     public String toString(){
-        return (name1 + " " + id + " " + age + " " + dept);
+        return (name + " " + id + " " + age + " " + dept);
     }
 }
