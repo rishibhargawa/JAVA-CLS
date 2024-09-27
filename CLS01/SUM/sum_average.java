0e1d781b92ade357cbb05b9e41c24dc81b03467e
Rishi Bhargawa <rishi.bhargawa23@st.niituniversity.in>
2024-09-27 22:34:51 +0530

    .

--- CLS01/SUM/sum_average.java
+++ CLS01/SUM/sum_average.java
@@ -1,18 +1,18 @@
 package SUM;
-//import java.util.Scanner;
+import java.util.Scanner;
 public class sum_average {
     public static void main(String[] args) {
         sumlogic obj=new sumlogic();
-        //Scanner sc=new Scanner(System.in);
-        //System.out.println("Enter your Numbers: ");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your Numbers: ");
         int arr[]={1,2,3,4,5}; //Array Declaration
-        //for(int i=0;i<arr.length;i++){ //Loop for Iteration
-           //arr[i]=sc.nextInt(); //Input in Array Indexes
-        //}
+        for(int i=0;i<arr.length;i++){ //Loop for Iteration
+           arr[i]=sc.nextInt(); //Input in Array Indexes
+        }
         int result =obj.sum(arr); //Defined result var to call method
         System.out.println("SUM is: "+result);
         System.out.println("Average is: "+result/(float)arr.length);
-        //sc.close();   
+        sc.close();   
     }
 }
 
